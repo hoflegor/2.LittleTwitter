2c10ce9340e8bbe313dbd29b55b020ca8510a384
hoflegor <tmsz.hof@gmail.com>
2017-09-19 21:25:57 +0200

    added conn.php

---
+++ conn.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "qweta";
+$baseName = "LittleTwitter";
+
+$conn = new mysqli($servername,
+                   $username,
+                   $password,
+                   $baseName);
+
+if ($conn->connect_error) {
+    die("<hr><strong>!!</strong>Connection failed. Error: <strong><ins>$conn->connect_error</ins>!!</strong><hr>");
+}
+echo("<strong>**</strong>Connected to database: <strong><ins>$baseName</ins></strong>.<hr>");
+
+$conn->close();
+$conn = null;
